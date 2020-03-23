22a4e8457a19c976a3be7476e23a084476c58053
Saikumo <2056162994@qq.com>
2020-03-23 12:51:27 +0800

    login feature

--- src/main/java/me/saikumo/community/provider/GithubProvider.java
+++ src/main/java/me/saikumo/community/provider/GithubProvider.java
@@ -1,4 +1,47 @@
 package me.saikumo.community.provider;
 
+import com.alibaba.fastjson.JSON;
+import me.saikumo.community.dto.AccessTokenDTO;
+import me.saikumo.community.dto.GithubUser;
+import okhttp3.*;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
 public class GithubProvider {
+    public String getAccessToken(AccessTokenDTO accessTokenDTO){
+        MediaType mediaType = MediaType.get("application/json; charset=utf-8");
+        OkHttpClient client = new OkHttpClient();
+
+        RequestBody body = RequestBody.create(mediaType, JSON.toJSONString(accessTokenDTO));
+        Request request = new Request.Builder()
+                .url("https://github.com/login/oauth/access_token")
+                .post(body)
+                .build();
+        try (Response response = client.newCall(request).execute()) {
+            String str =response.body().string();
+            String token = str.split("&")[0].split("=")[1];
+            return token;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public GithubUser getUser(String accessToken){
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url("https://api.github.com/user?access_token="+accessToken)
+                .build();
+        try (Response response = client.newCall(request).execute()) {
+            String str =  response.body().string();
+            GithubUser githubUser = JSON.parseObject(str,GithubUser.class);
+            return githubUser;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
